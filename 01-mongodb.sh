9324c0383c4c27dbedbe57ce4a288ca85d5cc64d
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-08 18:23:25 +0530

    api doc added

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -39,7 +39,7 @@ validate $? "Installing mongodb"
 systemctl --now enable mongod
 validate $? "Starting and Enabling MongoDB"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.config
+sed -i 's/127.0.0.1/0.0.0.0/g'  /etc/mongod.conf
 validate $? "Allowing remote connections to MongoDB"
 
 systemctl restart mongod
