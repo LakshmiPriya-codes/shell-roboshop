6fd03ed1f7c38ce57b5a0f0399370e9b895edd65
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-08 16:48:59 +0530

    api doc added

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -6,21 +6,22 @@ sudo mkdir -p / $LOG_FOLDER
 sudo chown -R ec2-user:ec2-user $LOG_FOLDER
 sudo chmod -R 755 $LOG_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
-TIMESTAMP= "$(date +%Y-%M-%S %H:%M:%S)"
-R="/e[31m"
-G="/e[32m"
-Y="/e[33m"
-N="/e[0m"
-
-
 USERID=$(id -u)
- if [ $USERID ne 0 ]; then
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+
+ if [ $USERID -ne 0 ]; then
   echo -e "$TIMESTAMP [Error] $R Please run this script with root access $N" | tee -a $LOGS_FILE 
   exit 1 
  fi  
 
 validate(){
-  if [ $1 ne 0 ]; then
+  if [ $1 -ne 0 ]; then
     echo -e "$TIMESTAMP [Error] $2 .... $R Failure $N" | tee -a $LOGS_FILE
     exit 1
   else
