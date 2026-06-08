92b0db23b321ebe446914b2d51d35f7f41bceeb3
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-08 16:41:32 +0530

    api doc added

---
+++ 01-mongodb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+LOG_FOLDER="/var/log/roboshop"
+sudo mkdir -p / $LOG_FOLDER
+sudo chown -R ec2-user:ec2-user $LOG_FOLDER
+sudo chmod -R 755 $LOG_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+TIMESTAMP= $(date +%Y-%M-%S %H:%M:%S)
+R="/e[31m"
+G="/e[32m"
+Y="/e[33m"
+N=R="/e[0m"
+
+
+USERID=$(id -u)
+ if [ $USERID ne 0 ]; then
+  echo -e "$TIMESTAMP [Error] $R Please run this script with root access $N" | tee -a $LOGS_FILE 
+  exit 1 
+ fi  
+
+validate(){
+  if [ $1 ne 0 ]; then
+    echo -e "$TIMESTAMP [Error] $2 .... $R Failure $N" | tee -a $LOGS_FILE
+    exit 1
+  else
+    echo -e " $TIMESTAMP [Info] $2 .... $G Success $N" | tee -a $LOGS_FILE
+ fi
+
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+validate $? "Adding mongo repo" 
+
