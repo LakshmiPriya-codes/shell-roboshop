8ca16997ce4764c0f00474c441635e02a0b27d31
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-09 18:01:55 +0530

    api doc added

--- 04-rabbitmq.sh
+++ 04-rabbitmq.sh
@@ -30,16 +30,16 @@ validate(){
 }
 
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
-validate $? "Adding rabbitmq repo"
+validate $? "Adding rabbitmq repo"  
 
-dnf install rabbitmq-server -y
+dnf install rabbitmq-server -y &>> $LOGS_FILE
 validate $? "Installing rabbitmq server"
 
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl enable rabbitmq-server &>> $LOGS_FILE
+systemctl start rabbitmq-server  &>> $LOGS_FILE
 validate $? "Enabling & Starting rabbitmq server"
 
 
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop roboshop123   &>> $LOGS_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGS_FILE
 validate $? "setting up username and password"
