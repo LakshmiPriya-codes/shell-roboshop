1002bd182eb40b10722b07bb38a9228851ac9f49
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-09 21:41:33 +0530

    api doc added

--- 05-catalogue.sh
+++ 05-catalogue.sh
@@ -68,5 +68,18 @@ validate $? "Created systemctl service"
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 validate $? "Added mongo repo"
 
-dnf install mongodb-mongosh -y
+dnf install mongodb-mongosh -y &>> $LOGS_FILE
 validate $? "Installed mongodb client"
+
+INDEX=$(mongosh --host mongodb.lpdaws.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -lt 0 ]; then
+    mongosh --host mongodb.lpdaws.online </app/db/master-data.js &>>$LOGS_FILE
+    VALIDATE $? "Load Products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl enable catalogue &>>$LOGS_FILE
+systemctl restart catalogue &>>$LOGS_FILE
+VALIDATE $? "Restarting catalogue"
