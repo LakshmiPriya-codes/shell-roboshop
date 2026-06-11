5ba86e67bf9e5d8d781134fdb865a4d9ea82c6a0
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-11 17:46:28 +0530

    äpi doc added

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -47,6 +47,50 @@ do
             )
             echo "Launched Instance: $INSTANCE_ID"
 
+
+            if [ $instance == "frontend" ]; then
+                    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+                    --query 'Reservations[*].Instances[*].PublicIpAddress' --output text
+                )
+     
+             R53_RECORD="$DOMAIN_NAME"
+
+                else
+                IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID\
+                --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text
+                ) 
+
+                R53_RECORD="$instance.$DOMAIN_NAME"
+
+            fi    
+
+
+             #####Updating Route53 records ######
+                    aws route53 change-resource-record-sets \
+                    --hosted-zone-id  $ZONE_ID \
+                    --change-batch  '
+
+                    {
+                        "Comment": "Update A record to new IP",
+                        "Changes": [
+                            {
+                                "Action": "UPSERT",
+                                "ResourceRecordSet": {
+                                    "Name": "'$R53_RECORD'",
+                                    "Type": "A",
+                                    "TTL":  1,
+                                        "ResourceRecords": [
+                                        {
+                                            "Value": "'$IP'"
+                                        }
+                                        ] 
+                                    } 
+                                }     
+                            ]
+                        } 
+                    '
+                echo "updated R53 record for: $instance"
+
         else
             echo "roboshop-$instance already running: $INSTANCE_ID"
         fi
