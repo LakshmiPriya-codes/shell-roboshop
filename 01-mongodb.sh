c0281eec4bbbbeff0257c3f59f3aca977986ef32
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-12 15:04:37 +0530

    api doc added

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -34,9 +34,10 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 validate $? "Adding mongo repo" 
 
 dnf install mongodb-org -y &>> $LOGS_FILE
-validate $? "Installing mongodb" 
+validate $? "Installing Mongodb" 
 
 systemctl --now enable mongod
+systemctl start mongod 
 validate $? "Starting and Enabling MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g'  /etc/mongod.conf
